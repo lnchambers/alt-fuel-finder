9b4a9729f41c7d1038bad03b96fec0341a9a452e
Luke Chambers <lukenatchambers@gmail.com>
2018-03-28 09:33:34 -0600

    Build out dummy tests and make API call in Postman

--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -10,13 +10,12 @@ feature "User can visit root page" do
   end
 
   it "I can search by zip code" do
-    # When I visit "/"
     visit "/"
-    # And I fill in the search form with 80203
+
     fill_in "q", with: "80203"
-    # And I click "Locate"
     click_on "Locate"
-    # Then I should be on page "/search" with parameters visible in the url
+
     expect(current_path).to eq("/search")
+    expect(page).to have_content("results")
   end
 end
--- spec/features/user_can_visit_the_search_path_spec.rb
+++ spec/features/user_can_visit_the_search_path_spec.rb
@@ -3,9 +3,19 @@ require "rails_helper"
 describe "As a user" do
   describe "when I visit the search page" do
     it "I see the attributes of each station" do
+      visit "/search"
+
+      fill_in "q", with: "80203"
+      click_on "Locate"
+
+      expect(page).to have_content("Name")
+      expect(page).to have_content("Address")
+      expect(page).to have_content("Fuel Types")
+      expect(page).to have_content("Distance")
+      expect(page).to have_content("Access Times")
       # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
     end
-    
+
     it "I can see a list of the 10 closest stations to 80203" do
       # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
     end
