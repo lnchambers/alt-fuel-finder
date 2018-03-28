6381590810bbac7ee0a91ce5a46f238ffd2110af
Luke Chambers <lukenatchambers@gmail.com>
2018-03-28 10:09:11 -0600

    Stub out requests

--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -10,6 +10,9 @@ feature "User can visit root page" do
   end
 
   it "I can search by zip code" do
+    json_response = File.open("./spec/fixtures/stations_by_zip.json")
+    stub_request(:get, "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV["NREL_API_KEY"]}&location=80203&radius=6&status=E&fuel_type=ELEC,LPG&limit=10")
+       .to_return(status: 200, body: json_response)
     visit "/"
 
     fill_in "q", with: "80203"
--- spec/features/user_can_visit_the_search_path_spec.rb
+++ spec/features/user_can_visit_the_search_path_spec.rb
@@ -1,9 +1,14 @@
 require "rails_helper"
 
 describe "As a user" do
+  before :all do
+    json_response = File.open("./spec/fixtures/stations_by_zip.json")
+    stub_request(:get, "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV["NREL_API_KEY"]}&location=80203&radius=6&status=E&fuel_type=ELEC,LPG&limit=10")
+       .to_return(status: 200, body: json_response)
+  end
   describe "when I visit the search page" do
     it "I see the attributes of each station" do
-      visit "/search"
+      visit "/"
 
       fill_in "q", with: "80203"
       click_on "Locate"
